a71a51b24d2736ddd35703e5f6a4f2926cba1ca6
Sassendes <sassendes@gmail.com>
2026-09-25 22:23:47 +0100

    Update ansible_deps.sh

--- scripts/ansible_deps.sh
+++ scripts/ansible_deps.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 apt update
-apt install -y ansible sshpass python3-pip
+apt install -y ansible sshpass python3-pip python3-bcrypt
 pip3 install kubernetes --break-system-packages
 ansible-galaxy collection install kubernetes.core
 echo ">> deps done"
